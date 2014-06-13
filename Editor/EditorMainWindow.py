d8b657e3ef568fc6ca6ad39970e076b50959406a
Thomas Loubiou <xabufr@gmail.com>
2014-06-13 15:50:26 +0200

    Fusion base de connaissance de bastien,

--- Editor/EditorMainWindow.py
+++ Editor/EditorMainWindow.py
@@ -79,7 +79,7 @@ class EditorMainWindow(QObject):
 
     def __delete_facts(self):
         self.__model.del_fact(self.__current_model_position)
-        self.__update_helpers()
+        self.__fill_facts()
 
     def __fact_changed(self, fact_item):
         if fact_item:
